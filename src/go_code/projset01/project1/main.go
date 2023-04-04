81555aac996b8c9502a09334234c52d2f00cc2fc
小张的电脑\13504 <13504360302qq.com>
2023-04-04 22:19:08 +0800

    面向对象写法

--- src/go_code/projset01/project1/main.go
+++ src/go_code/projset01/project1/main.go
@@ -1,22 +1,72 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	key := ""    //接收用户的输入
-	loop := true //跳出循环
+type FamilyAccount struct {
+	key  string //接收用户的输入
+	loop bool   //跳出循环
 	//账户余额
-	balance := 10000.0
+	balance float64
 	//每次收支的金额
-	money := 0.0
+	money float64
 	//每次收支的说明
-	note := ""
+	note string
 	//收支的明细
-	details := "收支\t账户金额\t收支金额\t说明"
+	details string
+}
 
-	//主菜单
+func (this *FamilyAccount) showDetails() {
+	fmt.Println("--------------当前收支明细--------------")
+	fmt.Println(this.details)
+}
+func (this *FamilyAccount) Revenue() {
+	fmt.Println("本次收入金额：")
+	fmt.Scanln(&this.money)
+	this.balance += this.money
+	fmt.Println("本次收入说明：")
+	fmt.Scanln(&this.note)
+	this.details += fmt.Sprintf("\n收入\t %v\t  	%v\t  	 %v", this.balance, this.money, this.note)
+}
+func (this *FamilyAccount) expenditures() {
+	fmt.Println("本次支出金额：")
+	fmt.Scanln(&this.money)
+	if this.money > this.balance {
+		fmt.Println("余额不足")
+	}
+	this.balance -= this.money
+	fmt.Println("本次支出说明")
+	fmt.Scanln(&this.note)
+	this.details += fmt.Sprintf("\n支出\t	%v\t  	%v\t  	 %v", this.balance, this.money, this.note)
+
+}
+func (this *FamilyAccount) quit() {
+	fmt.Println("确认退出 输入 y 或者 n：")
+	choice := ""
+	for {
+		fmt.Scanln(&choice)
+		if choice == "y" || choice == "n" {
+			break
+		}
+		fmt.Println("输入错误 请重新输入")
+	}
+	if choice == "y" {
+		this.loop = false
+	}
+	this.loop = false
+}
+func NewFamilyAccount() *FamilyAccount {
+	return &FamilyAccount{
+		key:     "",
+		loop:    true,
+		balance: 10000.0,
+		money:   0.0,
+		details: "收支\t账户金额\t收支金额\t说   明",
+	}
+
+}
+
+// 主菜单
+func (this *FamilyAccount) MainMenu() {
 	for {
 		fmt.Println("\n--------------家庭收支记账软件--------------")
 
@@ -27,49 +77,20 @@ func main() {
 
 		fmt.Println("请输入1-4： ")
 
-		fmt.Scanln(&key)
-
-		switch key {
+		fmt.Scanln(&this.key)
+		switch this.key {
 		case "1":
-			fmt.Println("--------------当前收支明细--------------")
-			fmt.Println(details)
+			this.showDetails()
 		case "2":
-			fmt.Println("本次收入金额：")
-			fmt.Scanln(&money)
-			balance += money
-			fmt.Println("本次收入说明：")
-			fmt.Scanln(&note)
-			details += fmt.Sprintf("\n收入\t %v\t  	%v\t  	 %v", balance, money, note)
-
+			this.Revenue()
 		case "3":
-			fmt.Println("本次支出金额：")
-			fmt.Scanln(&money)
-			if money > balance {
-				fmt.Println("余额不足")
-				break
-			}
-			balance -= money
-			fmt.Println("本次支出说明")
-			fmt.Scanln(&note)
-			details += fmt.Sprintf("\n支出\t	%v\t  	%v\t  	 %v", balance, money, note)
+			this.expenditures()
 		case "4":
-			fmt.Println("确认退出 输入 y 或者 n：")
-			choice := ""
-			for {
-				fmt.Scanln(&choice)
-				if choice == "y" || choice == "n" {
-					break
-				}
-				fmt.Println("输入错误 请重新输入")
-			}
-			if choice == "y" {
-				loop = false
-			}
-			loop = false
+			this.quit()
 		default:
 			fmt.Println("输入错误请从新输入")
 		}
-		if !loop {
+		if !this.loop {
 			break
 		}
 	}
